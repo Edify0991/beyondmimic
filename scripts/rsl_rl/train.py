8710068c16c309cf6e78a16c5ac6ef12cb5ced19
Edify0991 <439507039@qq.com>
2026-03-15 17:36:06 +0800

    modify

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -140,8 +140,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         artifact = api.artifact(registry_name)
         env_cfg.commands.motion.motion_file = str(pathlib.Path(artifact.download()) / "motion.npz")
         print(f"[INFO]: Using wandb registry motion: {args_cli.registry_name}")
-    else:
-        raise ValueError("Must provide either --motion_file or --registry_name for motion data.")
+    
 
     # specify directory for logging experiments
     log_root_path = os.path.join("logs", "rsl_rl", agent_cfg.experiment_name)
